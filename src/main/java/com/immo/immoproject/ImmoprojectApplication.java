acf911454cba409ab42f81a09214d6406138997e
Mehdi CHEKINI <mehdi.chekini@rte-france.com>
2018-10-16 20:34:48 +0200

    init project

---
+++ src/main/java/com/immo/immoproject/ImmoprojectApplication.java
@@ -0,0 +1,12 @@
+package com.immo.immoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmoprojectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/immo/immoproject/ImmoprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.immo.immoproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmoprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
